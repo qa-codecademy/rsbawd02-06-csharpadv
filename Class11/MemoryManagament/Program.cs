30eebf59ab9253a11385683ca0cfba4934146847
Almir Vuk <almir.vuk@outlook.com>
2024-06-04 22:37:25 +0200

    Class11 code added.

---
+++ Class11/MemoryManagament/Program.cs
@@ -0,0 +1,64 @@
+﻿using MemoryManagament.Utils;
+
+namespace MemoryManagament;
+
+public class Program
+{
+    public static void Main()
+    {
+        int a = 10;
+        int b = 20;
+
+        a = b;
+        b = 30; 
+
+        Console.WriteLine(a);
+        Console.WriteLine(b);
+
+        bool isEnabled = false;
+        char letter = 'a';
+
+        string sentence = "some value";
+
+        Student student = new Student();
+
+        student.FirstName = "Zoran";
+        student.LastName = "Malinovic";
+
+        // int broj = 10;
+        // var broj = 10;
+
+        // Student student2 = new Student
+        var student2 = new Student();
+
+        student2.FirstName = "Miroslav";
+
+        student2 = student;
+
+        student2.FirstName = "Almir";
+
+        Console.WriteLine(student.FirstName);
+        Console.WriteLine(student2.FirstName);
+
+        Student studentX = new Student();
+
+        Console.WriteLine(studentX.FirstName);
+
+        Console.WriteLine(".................................");
+
+        string path = "noviFajl.txt";
+
+        if (File.Exists(path) == false)
+            File.Create(path)
+                .Close();
+
+        OurTextWriter ourTextWriter = new OurTextWriter(path);
+
+        using (ourTextWriter)
+        {
+            ourTextWriter.WriteContent("We are learning C# we are happy!");
+        } // ourTextWriter.Dispose()   
+
+        ourTextWriter.WriteContent("Almir");
+    }
+}
---
+++ Class11/MemoryManagament/Utils/OurTextWriter.cs
@@ -0,0 +1,31 @@
+﻿namespace MemoryManagament.Utils
+{
+    public class OurTextWriter : IDisposable
+    {
+        private string _path;
+        private bool _disposedValue = false;
+
+        private StreamWriter _streamWriter;
+
+        public OurTextWriter(string path)
+        {
+            _path = path;
+
+            _streamWriter = new StreamWriter(path);
+        }
+
+        public void WriteContent(string content)
+        {
+            _streamWriter.Write(content);
+        }
+
+
+        // Here you can clean all the members and resources which you dont need
+        public void Dispose()
+        {
+            _streamWriter.Dispose();
+            _path = null;
+            _disposedValue = true;
+        }
+    }
+}
